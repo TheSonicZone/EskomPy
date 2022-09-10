db89a78f53c64ca6e0e68c629aa959c08bbd1d62
Miles T Prower <soniczoneyearbook@gmail.com>
2022-09-10 10:44:11 +0200

    Added a function to get the CPU type

--- main.py
+++ main.py
@@ -7,9 +7,31 @@
 
 import requests
 import simpleaudio as sa
+import os, platform, subprocess, re
+
 
 # Get Method Definitions (see API documentation techrad.co.za)
 GET_STATUS = "https://loadshedding.eskom.co.za/LoadShedding/GetStatus"
+
+
+def get_processor_name():
+    if platform.system() == "Windows":
+        return platform.processor()
+    elif platform.system() == "Darwin":
+        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+        command ="sysctl -n machdep.cpu.brand_string"
+        return subprocess.check_output(command).strip()
+    elif platform.system() == "Linux":
+        command = "cat /proc/cpuinfo"
+        all_info = subprocess.check_output(command, shell=True).decode().strip()
+        for line in all_info.split("\n"):
+            if "model name" in line:
+                return re.sub( ".*model name.*:", "", line,1)
+    return ""
+
+
+
+
 
 
 # Program begin
